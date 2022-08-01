083c81f064ef3f071887a07b1247ab1adf70dde2
Prisana Cheng <chengprisana@gmail.com>
2022-08-01 15:54:56 -0500

    updating api

--- src/api/index.js
+++ src/api/index.js
@@ -7,11 +7,33 @@ export const registerPerson = async (registeredUsername, registeredPassword) => 
         headers: { 'Content-Type': 'application/json'
         },
         body: JSON.stringify({
-            username: registeredUsername,
+            username:registeredUsername,
             password:registeredPassword    
         })
    })
    const result = await response.json()
    const token = result.token
    return token
+}
+
+export const loginUser = async (username, password) => {
+    try{
+        const response = await fetch (`${APIURL}/users/login`,
+        {
+            method:"POST",
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                username: username,
+                password: password
+            })
+        })
+        const result = await response.json()
+        const token = result.token
+        return token
+
+    } catch (error){
+        console.error('Trouble fetching users', error)
+    }
 }
