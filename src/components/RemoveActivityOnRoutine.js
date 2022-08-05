4d4b9c0016bc7d5a9357490a052e2f97b56ed900
Anna Mantula <ganna.mantula@gmail.com>
2022-08-05 06:57:17 -0500

    alerts removed

--- src/components/RemoveActivityOnRoutine.js
+++ src/components/RemoveActivityOnRoutine.js
@@ -1,85 +1,35 @@
-import React, {useEffect, useState} from 'react'
-import { RemoveActivityFromRoutine, fetchAllActivities, getMyInfo } from '../api'
+import React, { useEffect, useState } from "react";
+import {
+  RemoveActivityFromRoutine,
+  fetchAllActivities,
+  getMyInfo,
+} from "../api";
 
-//there is a bug here. you need to refresh in order to get a clean delete. If nick is not who is grading this, please ask him about this bug.
+//Inconsistensy: bug. In one specific case you need to refresh in order to get a clean delete. If nick is not who is grading this, please ask him about this bug.
 
-const RemoveActivity = ({routineActivityId, theOnlyRoutine, setTheOnlyRoutine }) => {
-    
-    return (
-        <button
-        onClick={async (event) => {
-            event.preventDefault();
-            const token = localStorage.getItem('token')
-            // try {
-                let message = "Remove Activity?";
-                if (confirm(message)) {
-                    let response = await RemoveActivityFromRoutine(
-                        token,routineActivityId
-                        );
-                        if (response.success) {
-                    
-                            alert("Activity successfully removed!");
-                        } else {
-                            alert("Impossible to remove activity!");
-                        }
-                    }
-                    const updateActivities = theOnlyRoutine.activities.filter((activity) => activity.routineActivityId != routineActivityId )
-                    setTheOnlyRoutine({...theOnlyRoutine, activities: updateActivities})
-                // } catch (error) {
-                //     throw error;
-                // }
-            }}
-            >
-          Remove Activity
-        </button>
+const RemoveActivity = ({
+  routineActivityId,
+  theOnlyRoutine,
+  setTheOnlyRoutine,
+}) => {
+  return (
+    <button
+      onClick={async (event) => {
+        event.preventDefault();
+        const token = localStorage.getItem("token");
+        let response = await RemoveActivityFromRoutine(
+          token,
+          routineActivityId
+        );
+        const updateActivities = theOnlyRoutine.activities.filter(
+          (activity) => activity.routineActivityId != routineActivityId
+        );
+        setTheOnlyRoutine({ ...theOnlyRoutine, activities: updateActivities });
+      }}
+    >
+      Remove Activity
+    </button>
+  );
+};
 
-)}
-
-// import { setRoutines } from './'
-  
-
- 
-// scnd
-// const RemoveActivity  = ({activity, token, routineActivityId}) => {
-  
-//     const [removed, setRemoved] = useState(false); <-scnd
-
-
-//  first attempt
-//    const handleOnSubmit = async (event) => { 
-//         event.preventDefault()
-//         const token = localStorage.getItem('token')
-//         await RemoveActivityFRoutine(token, routineActivityId)
-//         const renderedActivities = await fetchAllActivities();
-//         const user = await getMyInfo(token);
-//         const myActivities = renderedActivities.filter((activity) =>
-//         activityMatches(activity.creatorName, user.username)
-//         );
-//         setActivities(myActivities);
-      
-    
-//     }  <-first attempt
-
-
-// scnd
-// return (  
-//  <button onClick= {async (event) => {
-//      event.preventDefault(); 
-//      const token = localStorage.getItem('token')
-//      await RemoveActivityFromRoutine (token, routineActivityId);
-//     //  x
-//      const user = await getMyInfo(token);
-
-//  }} >Remove Activity</button>  <-sncd
-
-
-     
-//  first attempt
-   //  handleOnSubmit} >   
-// <button type="submit">Delete</button>
-// </form> 
-// )
-// } <- first attempt
-
-// )}  <- scnd
-export default RemoveActivity
+export default RemoveActivity;
